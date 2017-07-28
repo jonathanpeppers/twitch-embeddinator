0279fbf2ba9f0cb5ffcbceaf4c2e755d472fdcb1
Jonathan Peppers <jonathan.peppers@microsoft.com>
2017-07-28 09:44:51 -0500

    Changes to Java

--- AndroidStudio/app/src/main/java/com/xamarin/embedeverything/MainActivity.java
+++ AndroidStudio/app/src/main/java/com/xamarin/embedeverything/MainActivity.java
@@ -1,13 +1,34 @@
 package com.xamarin.embedeverything;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.app.*;
+import android.support.v4.app.FragmentActivity;
+import weatherapp_droid.MainFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        setContentView(weatherapp_droid.R.layout.main);
+
+        try {
+            FragmentTransaction ft = getFragmentManager().beginTransaction();
+            ft.replace(weatherapp_droid.R.id.fragment_frame_layout, new MainFragment(), "main");
+            ft.commit();
+        } catch (Throwable e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (getFragmentManager().getBackStackEntryCount() != 0) {
+            getFragmentManager().popBackStack();
+        }
+        else {
+            super.onBackPressed();
+        }
     }
 }
